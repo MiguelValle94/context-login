197e316ba6a7481df9740e941660dba7699daa10
miguelbachi5a <miguelbachi5a@gmail.com>
2020-12-02 19:16:35 +0100

    fix: app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import AuthenticatedRoute from './AuthenticatedRoute';
-import Home from './Home';
-import Login from './Login';
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import AuthenticatedRoute from './AuthenticatedRoute'
+import Home from './Home'
+import Login from './Login'
 
 function App() {
   return (
